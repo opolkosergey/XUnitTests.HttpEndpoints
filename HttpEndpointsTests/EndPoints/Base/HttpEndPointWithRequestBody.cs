9178b90e22a78051607ec33364c032ebe81da3d0
OpolkoSergey <opolkosergey@gmail.com>
2017-10-29 00:34:56 +0300

    Override CreateRequest for endpoints with request body

--- HttpEndpointsTests/EndPoints/Base/HttpEndPointWithRequestBody.cs
+++ HttpEndpointsTests/EndPoints/Base/HttpEndPointWithRequestBody.cs
@@ -1,4 +1,8 @@
-﻿namespace HttpEndpointsTests.EndPoints.Base
+﻿using System.Net.Http;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HttpEndpointsTests.EndPoints.Base
 {
     public abstract class HttpEndPointWithRequestBody<TRequestBodyModel, TReponseModel> : HttpEndPoint<TReponseModel> where TReponseModel : class
     {
@@ -9,6 +13,18 @@
             Body = requestBodyModel;
 
             return this;
+        }
+
+        protected override HttpRequestMessage CreateRequest()
+        {
+            var request = base.CreateRequest();
+
+            if (Body != null)
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(Body), Encoding.UTF8, "application/json");
+            }
+
+            return request;
         }
     }
 }
